4b5ce89c198a49480bfc74139662294a7e3c1862
Onne Bakker <onnebakker@gmail.com>
2019-08-14 16:07:24 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Hang Pang',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '+442073568339'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '+442073572739'
+  },
+  {
+    name:         'Pizza West',
+    address:      '712 Boundary St, London E2 7JE',
+    category:     'italian',
+    phone_number: '+442073577763'
+  },
+  {
+    name:         'Kebabi',
+    address:      '51A Shoreditch High St, London E1 6PQ',
+    category:     'belgian',
+    phone_number: '+442073568000'
+  },
+  {
+    name:         'Noodles Shop',
+    address:      '98 Boundary St, London E2 7JE',
+    category:     'japanese',
+    phone_number: '+442073568118'
+  },
+  {
+    name:         'Pasta Shop',
+    address:      '96A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '+442073666668'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
